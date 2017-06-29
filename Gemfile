df84cc68ef8d5bd8fb99c13fc6987d19120c8fde
Daniel Marino <starzonmyarmz@gmail.com>
2017-06-29 18:47:08 -0400

    Include scss by default

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 
 gem 'thin'
 gem 'sinatra'
-gem 'faker'         # http://rubydoc.info/github/stympy/faker/master/frames
+gem 'faker'
+gem 'sass'
--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
 require 'faker'
+require 'sass/plugin/rack'
 
 # Static Files
 set :public_folder, File.dirname(__FILE__) + '/public'
+
+# Sass
+Sass::Plugin.options[:syntax] = :scss
+Sass::Plugin.options[:style] = :compressed
+use Sass::Plugin::Rack
 
 # Root
 get '/' do
